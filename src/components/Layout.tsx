ed261fa632351358f0651a4e3c37f151fdaa7f62
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-04-08 21:03:15 +0200

    Add layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,11 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import Layout from "../components/Layout";
 import { VenueListSection } from "../components/VenueList";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1">
-        <VenueListSection />
-      </main>
-      <Footer />
-    </div>
+    <Layout>
+      <VenueListSection />
+    </Layout>
   );
 }
 
