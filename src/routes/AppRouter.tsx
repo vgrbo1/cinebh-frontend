a1e6ed0a947074f08a25c1a8a6b7cdc691da7495
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-04-09 15:21:28 +0200

    Add about page

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router";
+import About from "../pages/About";
 import Home from "../pages/Home";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
