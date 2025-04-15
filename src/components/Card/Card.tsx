f0313c7719282b1af9bb55eb57f16918e0cf5a52
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-04-15 23:49:04 +0200

    Add badge to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,11 +2,17 @@ export interface CardProps {
   imageUrl: string;
   title: string;
   secondaryText: string;
+  badgeText?: string;
 }
 
-export function Card({ imageUrl, title, secondaryText }: CardProps) {
+export function Card({ imageUrl, title, secondaryText, badgeText }: CardProps) {
   return (
-    <div className="rounded-xl shadow-md bg-white w-full p-4">
+    <div className="relative rounded-xl shadow-md bg-white w-full p-4">
+      {badgeText && (
+        <div className="absolute top-6 right-2 bg-secondary text-white text-sm font-semibold px-2 py-1.5 rounded-sm h-8">
+          {badgeText}
+        </div>
+      )}
       <img
         src={imageUrl}
         alt={title}
