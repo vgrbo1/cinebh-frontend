b2c14fbb1a3e9171db4b0929054c2543220cbe4c
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-04-16 15:52:03 +0200

    Adjust card styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,7 +9,7 @@ export function Card({ imageUrl, title, secondaryText, badgeText }: CardProps) {
   return (
     <div className="relative rounded-3xl border border-customGray shadow-light-100 bg-white w-full p-4">
       {badgeText && (
-        <div className="absolute top-6 right-2 bg-secondary text-white text-sm font-semibold px-2 py-1.5 rounded-sm h-8">
+        <div className="absolute top-6 right-2 bg-secondary text-white text-sm font-semibold px-2 py-1.5 rounded-lg h-8">
           {badgeText}
         </div>
       )}
