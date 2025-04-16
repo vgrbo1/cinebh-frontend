b55e3fb4e2737bcdc2a8d872659a326665d5b226
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-04-16 13:51:17 +0200

    Add scroll to top

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
+import { ScrollToTop } from "./util/ScrollTopTop.tsx";
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </QueryClientProvider>
