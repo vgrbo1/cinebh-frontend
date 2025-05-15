05aa6f242660b1ceed0e61cf72929f550cb01ca8
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-05-16 00:53:20 +0200

    Add signUp and verifyEmail calls

---
+++ src/services/authService.ts
@@ -0,0 +1,17 @@
+import { SignUpFormData } from "../validation/signUpSchema";
+import axiosInstance from "./axiosInstance";
+
+export const signUp = async (data: SignUpFormData) => {
+  const response = await axiosInstance.post("/api/public/auth/register", {
+    email: data.email,
+    password: data.password,
+  });
+  return response.data;
+};
+
+export const verifyEmail = async (token: string) => {
+  const response = await axiosInstance.post("/api/public/auth/email/confirm", {
+    emailVerificationToken: token,
+  });
+  return response.data;
+};
