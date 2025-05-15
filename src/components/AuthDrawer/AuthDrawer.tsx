896389ae0e0dee2cad1e0f9049d86a8a2e180876
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-05-15 13:41:11 +0200

    Modify auth drawer

--- src/components/AuthDrawer/AuthDrawer.tsx
+++ src/components/AuthDrawer/AuthDrawer.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/Logo.png";
 type AuthView = "login" | "signup" | "reset";
 
 interface AuthDrawerProps {
@@ -9,13 +11,50 @@ interface AuthDrawerProps {
   setView: (view: AuthView) => void;
 }
 
-const AuthDrawer: React.FC<AuthDrawerProps> = ({ isOpen, view, setView }) => {
+const AuthDrawer: React.FC<AuthDrawerProps> = ({
+  isOpen,
+  view,
+  setView,
+  onClose,
+}) => {
+  const [heading, setHeading] = useState("Welcome Back");
+  useEffect(() => {
+    const defaultHeadings: Record<AuthView, string> = {
+      login: "Welcome Back",
+      signup: "Hello",
+      reset: "Reset Password",
+    };
+    setHeading(defaultHeadings[view]);
+  }, [view]);
+
   return (
     <div
       className={`fixed top-20 right-0 h-full w-lg bg-primary shadow-lg transform transition-transform duration-300 z-50 ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
-    ></div>
+    >
+      <div className="w-full h-full flex flex-col items-center text-white px-4 py-8 font-primary">
+        <div className="w-full max-w-[400px] mb-8 flex justify-center">
+          <img src={logo} alt="Cinebh" className="h-8" />
+        </div>
+        <div className="relative w-full max-w-[400px] mb-6">
+          <button
+            type="button"
+            onClick={onClose}
+            className="absolute left-0 top-1 cursor-pointer"
+          >
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              className="text-customGray2 text-xl p-2 bg-customDarkCyanBlue rounded-md"
+            />
+          </button>
+
+          <h2 className="text-2xl font-bold text-customGray2 text-center">
+            {heading}
+          </h2>
+        </div>
+      </div>
+    </div>
   );
 };
 
