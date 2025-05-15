3fd6220352cb9e780cd8dd8ef6242124b9b759a5
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-05-15 12:56:12 +0200

    Update input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -24,9 +24,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <div
         className={`flex border items-center bg-white text-black text-base px-3 py-3 rounded-lg h-12 ${className}`}
       >
-        {prefixIcon && (
-          <div className="mr-2 text-customDarkCyanBlue">{prefixIcon}</div>
-        )}
+        {prefixIcon && <div className="mr-2 ">{prefixIcon}</div>}
 
         <input
           ref={ref}
