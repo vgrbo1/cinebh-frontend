9a44b83d7d9993db315d59d79c97156b9a635221
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-05-29 15:32:57 +0200

    fix typo

--- src/components/PasswordResetForm/StepNewPassword.tsx
+++ src/components/PasswordResetForm/StepNewPassword.tsx
@@ -125,7 +125,7 @@ export function StepNewPassword({
           disabled={changePasswordMutation.isPending}
         >
           {changePasswordMutation.isPending
-            ? "Reseting your password..."
+            ? "Resetting your password..."
             : "Continue"}
         </Button>
       </form>
