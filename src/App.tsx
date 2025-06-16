0033c87512a03fb2a9e410836794770c5e1f08c8
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-06-16 13:16:34 +0200

    Add toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
