4ab6cc4ab346161a195aeef3e9a7b2b0999ac9fd
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-06-16 12:29:18 +0200

    Rename type

--- src/types/model/Seat.ts
+++
@@ -1,9 +0,0 @@
-export interface Seat {
-  id: number;
-  label: string;
-  row: string;
-  column: number;
-  status: "AVAILABLE" | "SELECTED" | "RESERVED";
-  seatType: "REGULAR" | "VIP" | "LOVE";
-  price: number;
-}
