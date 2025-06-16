f3ae531dabc96b4ce37b52844f761a821018d99d
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-06-16 15:45:27 +0200

    Add reserve tickets page

---
+++ src/pages/ReserveTickets.tsx
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useParams } from "react-router";
+import { toast } from "react-toastify";
+import { SeatSelection } from "../components/SeatSelection/SeatSelection";
+import { reserveSeats } from "../services/movieProjectionService";
+import { SeatData } from "../types/model/SeatData";
+
+export function ReserveTickets() {
+  const { projectionId } = useParams();
+  const queryClient = useQueryClient();
+
+  const { mutate, status } = useMutation({
+    mutationFn: (seatIds: number[]) =>
+      reserveSeats(Number(projectionId), seatIds),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["projectionSeats", Number(projectionId)],
+      });
+      toast.success("Seats reserved successfully!");
+    },
+    onError: () => {
+      toast.error("Failed to reserve seats. Please try again.");
+    },
+  });
+
+  return (
+    <SeatSelection
+      actionLabel={status === "pending" ? "Reserving..." : "Make Reservation"}
+      onActionClick={(selectedSeats: SeatData[]) => {
+        const selectedSeatIds = selectedSeats.map((seat) => seat.id);
+        mutate(selectedSeatIds);
+      }}
+    />
+  );
+}
