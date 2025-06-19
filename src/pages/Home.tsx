a10e64da130a4329e7de16e7396047d59924dec8
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-06-19 22:49:54 +0200

    Add simple dialog

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,6 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { useState } from "react";
+import { useLocation } from "react-router";
 import { CurrentMovieList } from "../components/CardList/CurrentMovieList";
 import { UpcomingMovieList } from "../components/CardList/UpcomingMovieList";
 import { VenueList } from "../components/CardList/VenueList";
@@ -6,6 +9,12 @@ import { Layout } from "../components/Layout/Layout";
 import { VenueButtonRow } from "../components/VenueButtonRow/VenueButtonRow";
 
 export function Home() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const successParam = queryParams.get("success");
+
+  const [isDialogOpen, setIsDialogOpen] = useState(successParam === "true");
+
   return (
     <Layout>
       <Carousel />
@@ -13,6 +22,22 @@ export function Home() {
       <CurrentMovieList />
       <UpcomingMovieList />
       <VenueList />
+      <div>
+        <Dialog.Root open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+          <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+          <Dialog.Portal>
+            <Dialog.Content className="fixed font-primary left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/4 rounded-md bg-white p-8 ">
+              <h2 className="text-xl font-bold text-primary mb-4">
+                Payment Successful!
+              </h2>
+              <p className="text-center max-w-sm text-sm text-customDarkGray">
+                The receipt and ticket have been sent to your email. You may
+                retrieve them later from your User Profile.
+              </p>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </div>
     </Layout>
   );
 }
