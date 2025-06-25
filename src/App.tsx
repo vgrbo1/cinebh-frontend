fc0b826de9847cb1ad3ef784af4de8b0c68b4478
Vedad Grbo <92470181+vgrbo1@users.noreply.github.com>
2025-06-25 19:35:35 +0200

    Fix ws endpoint (#32)

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ import "./App.css";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
+  const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
   return (
     <>
-      <StompSessionProvider
-        url={
-          import.meta.env.VITE_API_URL || "http://localhost:8080" + "/api/ws"
-        }
-      >
+      <StompSessionProvider url={`${API_BASE}/api/ws`}>
         <AppRouter />
         <ToastContainer />
       </StompSessionProvider>
